91ad202a52967ec9700e2c7c0c2866803c97887c
Loouis Low <loouis@gmail.com>
2020-04-14 02:40:05 +0800

    separate raw and minified outputs with `gulp-rename` node module

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,39 @@ const autoPrefixer = require('autoprefixer')
 const gulp = require('gulp')
 const gutil = require('gulp-util')
 const postCss = require('gulp-postcss')
+const rename = require('gulp-rename')
 const sass = require('gulp-sass')
 const sassGlob = require('gulp-sass-glob')
 
+const { version } = require('./package.json')
+
 
 // ...minify/preprocess scss
+
+// outputStyle: `expanded` for debugging,
+// `compressed` for production.
+
 const srcScssPath = 'src/yogurt.scss'
 const distCssPath = 'dist'
-gulp.task('sass', () => {
+gulp.task('sass-min', () => {
   return gulp.src(srcScssPath)
     .pipe(sassGlob())
-    // outputStyle: `expanded` for debugging,
-    // `compressed` for production.
     .pipe(sass({ outputStyle: 'compressed' })
       .on('error', sass.logError))
     .pipe(postCss([autoPrefixer()]))
+    // .pipe(rename('yogurt-' + version + '_solidcore.min.css'))
+    .pipe(rename('yogurt.min.css'))
+    .pipe(gulp.dest(distCssPath))
+})
+
+
+gulp.task('sass-raw', () => {
+  return gulp.src(srcScssPath)
+    .pipe(sassGlob())
+    .pipe(sass({ outputStyle: 'expanded' })
+      .on('error', sass.logError))
+    // .pipe(rename('yogurt-' + version + '_solidcore.css'))
+    .pipe(rename('yogurt.css'))
     .pipe(gulp.dest(distCssPath))
 })
 
@@ -25,13 +43,17 @@ gulp.task('sass', () => {
 const watchSrcScssPath = 'src/**/**/**/**/*.scss'
 gulp.task('watch', gulp.series([
 
-    'sass',
+    'sass-raw',
+    'sass-min',
 
   ], () => {
 
     gulp.watch(watchSrcScssPath,
-      gulp.series(['sass']))
+      gulp.series([
+        'sass-raw',
+        'sass-min'
+      ])
+    )
 
   })
-
 )
